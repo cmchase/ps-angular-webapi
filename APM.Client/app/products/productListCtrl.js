c9d860a65e0cdbf80d47cff63a66d280a0bc5cd7
unknown <Cory.Chase@RANCCCHASEW7L.corp.peopleclick.com>
2015-06-11 15:05:30 -0400

    First commit

---
+++ APM.Client/app/products/productListCtrl.js
@@ -0,0 +1,17 @@
+(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("ProductListCtrl",
+                     ['productResource', ProductListCtrl]);
+
+    function ProductListCtrl(productResource) {
+        var vm = this;
+
+        vm.searchCriteria = "GDN";
+        
+        productResource.query(function (data) {
+            vm.products = data;
+        });
+    }
+}());
